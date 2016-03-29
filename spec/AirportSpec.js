f99323db31fdc6b2f1c532b516da989631fba417
olmesm <olmesm@gmail.com>
2016-03-29 17:04:48 +0100

    added take off function

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -8,9 +8,17 @@ describe("Airport", function() {
   });
 
   describe("landing", function() {
-    it("lands a plane", function() {
+    it("adds a plane", function() {
       airport.land(plane);
-      expect(airport.planes()).toEqual([plane]);
+      expect(airport.planes()).toContain(plane);
+    });
+  });
+
+  describe("take off", function() {
+    it("removes the plane", function() {
+      airport.land(plane);
+      airport.takeOff(plane);
+      expect(airport.planes()).not.toContain(plane);
     });
   });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,11 +1,15 @@
 function Airport() {
   hanger = new Array();
-};
+}
 
 Airport.prototype.land = function(plane) {
   hanger.push(plane);
-};
+}
 
 Airport.prototype.planes = function() {
   return hanger;
-};
+}
+
+Airport.prototype.takeOff = function() {
+  hanger.pop();
+}
